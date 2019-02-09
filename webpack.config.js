ac0d565fabbaa39445a731de475ebcf1fd51fb8c
Andrew <andrewahnjr@gmail.com>
2019-02-09 15:59:52 -0800

    Added woff2 to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 module.exports = {
-  entry: __dirname + '/client/src/index.jsx',
+  entry: path.resolve(__dirname, './client/src/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist')
+  },
   module: {
     rules: [
       {
@@ -11,11 +17,16 @@ module.exports = {
             presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        loader:
+          'style-loader!css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/client/dist'
   }
 };
