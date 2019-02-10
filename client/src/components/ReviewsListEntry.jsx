8880bf9e2d93f843fda98d09308aaec0066b98c8
Andrew <andrewahnjr@gmail.com>
2019-02-09 17:07:20 -0800

    Added styling for each review entry

--- client/src/components/ReviewsListEntry.jsx
+++ client/src/components/ReviewsListEntry.jsx
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StarRatings from 'react-star-ratings';
 import styles from '../styles/reviewListEntry.css';
 
-const ReviewsListEntry = props => {
-  return (
-    <div>
-      <div className={styles.feedTop}>
-        <StarRatings
-          rating={props.review.rating}
-          starDimension="14px"
-          starSpacing="0.1px"
-          starRatedColor="black"
-        />
-        <div className={styles.date}>{props.review.review_date}</div>
+class ReviewsListEntry extends Component {
+  constructor(props) {
+    super(props);
+    // this.recommendChecker = this.recommendChecker.bind(this);
+  }
+
+  // recommendChecker() {
+  //   console.log('in here', this.props.review.rating);
+  //   if (this.props.rating >= 4) {
+  //     return <div>I recommend this product</div>;
+  //   } else {
+  //     return <div />;
+  //   }
+  // }
+
+  render() {
+    return (
+      <div>
+        <br />
+        <br />
+        <div className={styles.feedTop}>
+          <StarRatings
+            rating={this.props.review.rating}
+            starDimension="14px"
+            starSpacing="0.1px"
+            starRatedColor="black"
+          />
+          <div className={styles.date}>{this.props.review.review_date}</div>
+        </div>
+        <br />
+        <div className={styles.header}>{this.props.review.header}</div>
+        <br />
+        <div className={styles.description}>
+          {this.props.review.description}
+        </div>
+        <br />
+        <div className={styles.userContainer}>
+          {/* <div>{this.recommendChecker()}</div> */}
+          <div className={styles.user}>{this.props.review.user}</div>
+          <div className={styles.verified}> - Verified Purchaser</div>
+        </div>
+        <br />
+        <div className={styles.bottomEntry}>
+          <div className={styles.reply}>Reply</div>
+          <div className={styles.helpful}>
+            <div className={styles.helpfulMsg}>Was this review helpful?</div>
+            <div className={styles.yes}>Yes</div>
+            <div className={styles.yesCount}>(0)</div>
+            <div className={styles.no}>No</div>
+            <div className={styles.noCount}>(0)</div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <hr />
       </div>
-      <div className={styles.header}>{props.review.header}</div>
-      <br />
-      <div>{props.review.description}</div>
-      <br />
-      <div>{props.review.user} - Verified Purchaser</div>
-      <br />
-      <div className={styles.bottomEntry}>
-        <div>Reply</div>
-        <div>Was this review helpful? Yes (0) No (0)</div>
-      </div>
-      <hr />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ReviewsListEntry;
