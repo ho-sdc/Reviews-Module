8b97d1a5679933b1f83fc13f95475a8a83424033
Andrew <andrewahnjr@gmail.com>
2019-02-12 21:07:22 -0800

    Set up express routes for filter

--- server/index.js
+++ server/index.js
@@ -28,3 +28,27 @@ app.get('/reviews/:review_id', (req, res) => {
     .then(data => res.status(200).send(data))
     .catch(error => res.status(404).send(error));
 });
+
+app.get('/reviews/:review_id/helpful', (req, res) => {
+  let { review_id } = req.params;
+  Reviews.find({ review_id })
+    .$where('this.yes > this.nope')
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+});
+
+app.get('/reviews/:review_id/relevant', (req, res) => {
+  let { review_id } = req.params;
+  Reviews.find({ review_id })
+    .$where('this.yes + this.nope >= 110')
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+});
+
+app.get('/reviews/:review_id/newest', (req, res) => {
+  let { review_id } = req.params;
+  Reviews.find({ review_id })
+    .sort({ date: -1 })
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+});
