5d79355500c7f6ee4e10e4252d582fa4b0440355
Andrew <andrewahnjr@gmail.com>
2019-02-16 17:12:34 -0800

    Gave buttons appropriate styling

--- client/src/components/Buttons.jsx
+++ client/src/components/Buttons.jsx
@@ -11,6 +11,9 @@ const Buttons = props => {
         <div className={styles.load}>LOAD MORE</div>
         <div className={styles.arrow}>&#10230;</div>
       </div>
+      <div className={styles.container2}>
+        <div className={styles.write2}>WRITE A REVIEW &#10230;</div>
+      </div>
     </div>
   );
 };
