4645571651d595f55f870fa0627c0027b17558ff
Xialin Leung <xleung@Xialins-MacBook-Pro.local>
2019-03-01 12:39:40 -0800

    restructured the datagenerator and the data structure to create 1M primary products

--- database/data.js
+++ database/data.js
@@ -13,12 +13,10 @@ const {
   helpfulCountGen
 } = require('./generator.js');
 
-let reviews = [];
-
-generateReviews = n => {
-  for (let i = 0; i < n; i++) {
+generateReviews = () => {
+  let reviews = [];
+  for (let i = 0; i < Math.max(Math.floor(Math.random()*15), 5); i++) {
     let review = {
-      review_id: idGenerator(20),
       user: faker.internet.userName(),
       header: headerGenerator(),
       description: descriptionGenerator(),
@@ -32,24 +30,25 @@ generateReviews = n => {
       yes: helpfulCountGen(),
       nope: helpfulCountGen()
     };
+    reviews.push(review);
     // reviews.push(review);
-    return review;
   }
+  return reviews;
 };
 
 for ( let x = 0 ; x < 1; x++) {
   let writeStream = fs.createWriteStream(`./dataSet/data.csv`)
   
   function writeTenMillion(writer, callback) {
-    let i = 10000000
+    let i = 0
     function write () {
       let ok = true;
       do {
-        i--;
-        if ( i === 0 ){
-          writer.write( JSON.stringify(generateReviews(1)), callback);
+        i++;
+        if ( i === 10000000 ){
+          writer.write( JSON.stringify({productId: i, reviews: generateReviews()}), callback);
         } else {
-          ok = writer.write(JSON.stringify(generateReviews(1)));
+          ok = writer.write(JSON.stringify({productId: i, reviews: generateReviews()}));
         }
       } while (i > 0 && ok);
       if (i > 0){
